c7638c80facdb874dcdd21b20756cf51c055051b
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-11-19 16:35:46 -0800

    [add] Added NewPage functinality

--- wiki/forms.py
+++ wiki/forms.py
@@ -2,8 +2,40 @@ from django import forms
 from wiki.models import Page
 
 
-class PageForm(forms.Form):
-    """ Render and process a form based on the Page model. """
+class PageForm(forms.ModelForm):
+    """Render and process a form based on the Page model."""
 
-    title = forms.CharField(label="Your name", max_length=200)
-    content = forms.CharField(help_text="Write the content of your page here.")
+    class Meta:
+        """Meta class to specify form."""
+
+        model = Page
+        exclude = ('author', 'slug', 'modified', 'created')
+        # fields = ('title', 'content')
+
+    def __init__(self, *args, **kwargs):
+
+        print("\nI am in INIT in PageForm\n")
+
+        self.author = kwargs.pop('author')
+        # self.modified = kwargs.pop('modified')
+        self.created = kwargs.pop('created')
+
+        print("Author in page_form: {}".format(self.author))
+        print("Created in page_form: {}".format(self.created))
+
+        super(PageForm, self).__init__(*args, **kwargs)
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if Page.objects.filter(author=self.author, title=title).exists():
+            raise forms.ValidationError("You have already written a book with same title.")
+
+        return title
+
+
+    # title = forms.CharField(label="Your name", max_length=200)
+    # content = forms.CharField(help_text="Write the content of your page here.")
+
+class FriendlyForm(forms.Form):
+    first_name = forms.CharField(max_length=200)
+    last_name = forms.CharField(max_length=200)
--- wiki/views.py
+++ wiki/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
 from django.http import HttpResponseRedirect
+from django.utils import timezone
+from django.urls import reverse_lazy
 
 
 from wiki.models import Page
@@ -9,41 +12,76 @@ from wiki.forms import PageForm
 
 
 class PageListView(ListView):
-    """ Renders a list of all Pages. """
+    """Render a list of all Pages."""
+
     model = Page
 
     def get(self, request):
-        """ GET a list of Pages. """
+        """GET a list of Pages."""
+
         pages = self.get_queryset().all()
         return render(request, 'list.html', {
-          'pages': pages
+            'pages': pages
         })
 
 class PageDetailView(DetailView):
-    """ Renders a specific page based on it's slug."""
+    """Render a specific page based on it's slug."""
+
     model = Page
 
     def get(self, request, slug):
-        """ Returns a specific wiki page by slug. """
+        """Return a specific wiki page by slug."""
+
         page = self.get_queryset().get(slug__iexact=slug)
         return render(request, 'page.html', {
-          'page': page
+            'page': page
         })
 
 
-def get_page(request):
-    if request.method == 'POST':
-        f = PageForm(request.POST)
+class PageCreateView(CreateView):
+    template_name = 'newpage.html'
+    form_class = PageForm
 
-        print("Form: {}".format(f))
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.created = timezone.now()
+        self.object.modified = timezone.now()
+        self.object.save()
+        return HttpResponseRedirect(reverse_lazy('wiki-list-page'))
+
+    def get_form_kwargs(self, *args, **kwargs):
+        print("\nI am in get_form_kwargs in PageCreateView\n")
+
+        kwargs = super(PageCreateView, self).get_form_kwargs(*args, **kwargs)
+        kwargs['author'] = self.request.user
+        kwargs['created'] = timezone.now()
+
+        print("kwargs -> author: {}".format(kwargs['author']))
+        print("kwargs -> created: {}".format(kwargs['created']))
+
+        return kwargs
+
+
+
+class PageCreateView_(CreateView):
+    """Process create new_page logic."""
+
+    def get(self, request, *args, **kwargs):
+        context = {'form': PageForm}
+        return render(request, 'newpage.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = PageForm(request.POST)
+        print("Form: {}".format(form))
 
         if form.is_valid():
-            print("form is valid")
-            return HttpResponseRedirect("/")
-        else:
-            print("form is invalid")
+            print("Form is valid")
+            page = form.save(commit=False)
+            page.author = request.user
+            page.created = timezone.now()
+            page.modified = timezone.now()
+            page.save()
+            return HttpResponseRedirect(reverse_lazy('wiki-list-page'))
 
-    else:
-        form = PageForm()
-
-    return render(request, 'newpage.html', {'form': form})
+        return render(request, 'newpage.html', {'form': form})
